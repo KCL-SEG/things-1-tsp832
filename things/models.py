214fb12f95c3abcc847be65845717ca7ea72b3db
Sparsh Patel <sparsh.patel@kcl.ac.uk>
2022-10-14 19:32:17 +0100

    Added Thing model restrictions

--- things/models.py
+++ things/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(AbstractBaseUser):
     name = models.CharField(unique = True, max_length = 30, blank = False)
-    description = models.TextField(unique = False, max_length = 120, blank = True)
+    description = models.CharField(unique = False, max_length = 120, blank = True)
     quantity = models.PositiveSmallIntegerField(unique = False, validators=[MinValueValidator(0), MaxValueValidator(100)])
     USERNAME_FIELD = 'name'
     password = models.TextField(blank = True)
