56e1c088571b472974a96be3c7215785d6e6c141
Sparsh Patel <sparsh.patel@kcl.ac.uk>
2022-10-14 18:49:20 +0100

    Added Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class Thing(AbstractUser):
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
