ef8586d2e56f0af576e822753e3e120bde3821b6
Sparsh Patel <sparsh.patel@kcl.ac.uk>
2022-10-14 19:03:48 +0100

    Added Thing model restrictions

--- things/models.py
+++ things/models.py
@@ -1,8 +1,10 @@
+from sys import maxsize
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(AbstractUser):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(unique = True, max_length = 30, blank = False)
+    description = models.TextField(unique = False, max_length = 120, blank = True)
+    quantity = models.PositiveSmallIntegerField(unique = False, validators=[MinValueValidator(0), MaxValueValidator(100)])
